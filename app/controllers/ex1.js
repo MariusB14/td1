b15686177ef207df0570a01484be8fdea05be34e
Marius <mariusboulier@gmail.com>
2021-01-26 16:28:47 +0100

    deuxième commit

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -4,13 +4,29 @@ import {tracked} from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
   @tracked content = 'Entrez votre texte';
+  @tracked info = '';
   MAX=100;
 
   get size(){
     return this.MAX - this.content.length;
   }
 
+  get style(){
+    if(this.size > 50){
+      return "info";
+    }
+    if(this.size > 20){
+    return "warning";
+    }
+    return "danger";
+  }
+
   @action save(){
     console.log('Test save' + this.content);
   }
+
+  @action clear(){
+    this.content = '';
+  }
+
 }
