240e19ba0efc42e8743d0b7314f5feab6adbd52d
Marius <mariusboulier@gmail.com>
2021-02-09 14:10:24 +0100

    commit pour le helper

--- app/helpers/plural.js
+++ app/helpers/plural.js
@@ -4,9 +4,9 @@ export default helper(function plural(params/*, hash*/) {
   let [count, zero, one, other] = params;
   switch(count){
     case 0:
-      return zero;
-    case 1:
       return "Aucun service sélectionné";
+    case 1:
+      return "";
   }
   return params;
 });
